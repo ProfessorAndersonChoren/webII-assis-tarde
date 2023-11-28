ba01b46e0ab6aab469dde23580d41e2ec2a17cb4
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-28 15:59:23 -0300

    Sistema de chamados - Script do banco

--- Sistema de chamados/script.sql
+++ Sistema de chamados/script.sql
@@ -1,25 +1,112 @@
-create database if not exists call_assis_afternoon;
-use call_assis_afternoon;
-create table if not exists users(
-	id tinyint unsigned primary key auto_increment,
-    name varchar(20) not null,
-    email varchar(30) not null,
-    password char(60) not null
-);
-create table if not exists equipments(
-	pc_number varchar(10) primary key,
-    floor tinyint unsigned not null,
-    room tinyint unsigned not null
-);
-create table if not exists calls(
-	id tinyint unsigned primary key auto_increment,
-    open_date date not null,
-    last_modify_date date null,
-    user_id tinyint unsigned not null,
-    equipment_id varchar(10) not null,
-    classification tinyint not null,
-    description text not null,
-    notes text null,
-    constraint fk_user foreign key (user_id) references users(id),
-    constraint fk_equipment foreign key (equipment_id) references equipments(pc_number)
-);
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: call_assis_afternoon
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `calls`
+--
+
+DROP TABLE IF EXISTS `calls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `calls` (
+  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `open_date` date NOT NULL,
+  `last_modify_date` date DEFAULT NULL,
+  `user_id` tinyint unsigned NOT NULL,
+  `equipment_id` varchar(10) NOT NULL,
+  `classification` tinyint NOT NULL,
+  `description` text NOT NULL,
+  `notes` text,
+  PRIMARY KEY (`id`),
+  KEY `fk_user` (`user_id`),
+  KEY `fk_equipment` (`equipment_id`),
+  CONSTRAINT `fk_equipment` FOREIGN KEY (`equipment_id`) REFERENCES `equipments` (`pc_number`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calls`
+--
+
+LOCK TABLES `calls` WRITE;
+/*!40000 ALTER TABLE `calls` DISABLE KEYS */;
+INSERT INTO `calls` VALUES (2,'2023-11-23',NULL,1,'ABC123',1,'Isso vai dar erro',NULL),(3,'2023-11-28',NULL,1,'ABC123',3,'Um teste de chamado',NULL),(4,'2023-11-28',NULL,1,'ABC123',3,'Um teste de chamado',NULL),(6,'2023-11-28',NULL,1,'ABC123',3,'Um teste',NULL),(7,'2023-11-28',NULL,1,'ABC123',3,'Um teste de chamado',NULL);
+/*!40000 ALTER TABLE `calls` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipments`
+--
+
+DROP TABLE IF EXISTS `equipments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipments` (
+  `pc_number` varchar(10) NOT NULL,
+  `floor` tinyint unsigned NOT NULL,
+  `room` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`pc_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipments`
+--
+
+LOCK TABLES `equipments` WRITE;
+/*!40000 ALTER TABLE `equipments` DISABLE KEYS */;
+INSERT INTO `equipments` VALUES ('ABC123',2,202);
+/*!40000 ALTER TABLE `equipments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` char(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Maria','maria@gmail.com','$2y$10$RHo2g5UmT6wru1ePc3XEyezkVjd1DS.6rMc0lRB/K63/zo6dzjY22');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-28 15:59:04
