095abb2010d5956418b3098e3b5a3f38cc860847
ProfessorAndersonChoren <anderson.choren@faqi.edu.br>
2023-11-28 16:57:42 -0300

    Sistema de chamados - Ações da tela de listagem de chamados

--- Sistema de chamados/src/View/list-of-calls.php
+++ Sistema de chamados/src/View/list-of-calls.php
@@ -6,16 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Desk Help - Lista de chamados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 
 <body class="m-5">
     <nav class=" bg-info d-flex justify-content-between p-3">
-    <div>
-        <a href="dashboard.php" class="text-white text-decoration-none">Dashboard</a>
-        <a href="add-new-call.php" class="text-white text-decoration-none">Abrir chamado</a>
-        <a href="../Controller/Call.php?operation=findAll" class="text-white text-decoration-none">Listar chamados</a>
-    </div>
-    <a href="../Controller/Auth.php?operation=logout" class="text-white text-decoration-none">Sair</a>
+        <div>
+            <a href="dashboard.php" class="text-white text-decoration-none">Dashboard</a>
+            <a href="add-new-call.php" class="text-white text-decoration-none">Abrir chamado</a>
+            <a href="../Controller/Call.php?operation=findAll" class="text-white text-decoration-none">Listar chamados</a>
+        </div>
+        <a href="../Controller/Auth.php?operation=logout" class="text-white text-decoration-none">Sair</a>
     </nav>
     <main class="d-flex justify-content-center mt-3">
         <table class="table table-primary mx-5">
@@ -56,7 +57,12 @@
                             <?= (!empty($call["notes"])) ? $call["notes"] : "Não existem observações" ?>
                         </td>
                         <td>
-                            
+                            <a href="../Controller/Call.php?operation=findOne&code=<?= $call["id"] ?>" class="btn btn-warning" title="Editar o chamado">
+                                <i class="bi bi-pencil"></i>
+                            </a>
+                            <a href="../Controller/Call.php?operation=delete&code=<?= $call["id"] ?>" class="btn btn-danger" title="Deletar o chamado">
+                                <i class="bi bi-trash"></i>
+                            </a>
                         </td>
                     </tr>
                 <?php
